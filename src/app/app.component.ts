476b6f76fd015ca86915b3d2e874114665f42565
meezan-mallick <meezan.mallick@gmail.com>
2021-02-01 23:10:18 +0530

    GET request handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,47 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit,  VERSION } from '@angular/core';
-import { subscribeOn } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, VERSION } from "@angular/core";
+import { subscribeOn } from "rxjs/operators";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
 
-  constructor(private http:HttpClient){
+  constructor(private http: HttpClient) {}
 
-  }
-
-  ngOnInit(){
-    
+  ngOnInit() {
+    this.FetchPosts();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     // console.log(postData);
-    this.http.post("https://angular-test-b2acb-default-rtdb.firebaseio.com/post.json",
-    postData).subscribe(responseData =>{
-      console.log(responseData)
+    this.http
+      .post(
+        "https://angular-test-b2acb-default-rtdb.firebaseio.com/post.json",
+        postData
+      )
+      .subscribe(responseData => {
+        console.log("ost saved");
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.FetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  FetchPosts()
+  {
+    this.http.get("https://angular-test-b2acb-default-rtdb.firebaseio.com/post.json").subscribe(posts => {
+      console.log(posts);
     });
   }
 }
